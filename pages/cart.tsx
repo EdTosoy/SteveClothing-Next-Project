afab72a43f99fca38603014326c7602cb2917da1
EdTosoy <edberto.tosoy@gmail.com>
2021-02-11 10:10:27 +0800

    update SEO

--- pages/cart.tsx
+++ pages/cart.tsx
@@ -2,10 +2,18 @@ import Cart from "components/Cart";
 import Footer from "components/Footer";
 import Header from "components/Header";
 import React from "react";
+import Head from 'next/head';
 
 export default function cart() {
   return (
     <div>
+      <Head>
+        <title>Addstract | Cart</title>
+        <meta
+          name="description"
+          content="Our menu is constantly evolving, yet always abides by a few simple constants. All of our cooking revolves around open wood fire. We work with a small group of fisherman, hunters, gatherers, ranchers, and farmers to find and follow microclimates that produce specific tastes from wild foods. Lastly, we steer clear of imported and commercial goods, opting instead to build our own pantry through patient harvesting and preservation."
+        />
+      </Head>
       <Header />
       <Cart />
       <Footer />
--- pages/products.tsx
+++ pages/products.tsx
@@ -2,10 +2,18 @@ import Footer from "components/Footer";
 import Header from "components/Header";
 import Products from "components/products";
 import React from "react";
+import Head from "next/head";
 
 export default function products() {
   return (
     <div>
+      <Head>
+        <title>Addstract | Shop</title>
+        <meta
+          name="description"
+          content="Our menu is constantly evolving, yet always abides by a few simple constants. All of our cooking revolves around open wood fire. We work with a small group of fisherman, hunters, gatherers, ranchers, and farmers to find and follow microclimates that produce specific tastes from wild foods. Lastly, we steer clear of imported and commercial goods, opting instead to build our own pantry through patient harvesting and preservation."
+        />
+      </Head>
       <Header />
       <Products />
       <Footer />
